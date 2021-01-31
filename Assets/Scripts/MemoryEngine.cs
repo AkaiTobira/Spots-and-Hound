12bc4804c8f404dec836f2bf332bbe83585f44a7
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2021-01-31 02:33:18 +0100

    Memory Sounds

--- Assets/Scripts/MemoryEngine.cs
+++ Assets/Scripts/MemoryEngine.cs
@@ -25,6 +25,7 @@ public class MemoryEngine : MonoBehaviour
     public void Selected( CardController controller, string selected){
         if( Locked ) return;
         controller.ShowCard();
+        AudioSystem.Instance.PlayEffect( "card_hide", 0.5f, true );
         if( _cleared == transform.childCount-1){
             StartCoroutine( OnFinish() );
         }
@@ -43,6 +44,7 @@ public class MemoryEngine : MonoBehaviour
     }
 
     private IEnumerator OnFinish(){
+        AudioSystem.Instance.PlayEffect( "shock", 0.5f, true );
         yield return new WaitForSeconds(1f);
         _dialogueSystem.ForceNextOption( _nextDialogue );
         _dialogueSystem.ProcessNextDialogue();
@@ -55,7 +57,7 @@ public class MemoryEngine : MonoBehaviour
         _selectedController = null;
         Locked = false;
         _cleared += 2;
-
+        AudioSystem.Instance.PlayEffect( "card_clear", 0.5f, true );
     }
 
     private IEnumerator DelayHide(CardController controller){
@@ -65,7 +67,7 @@ public class MemoryEngine : MonoBehaviour
         _selectedController = null;
         Locked = false;
 
-        AudioSystem.Instance.PlayEffect( "card_hide", 0.5f, true );
+        //AudioSystem.Instance.PlayEffect( "card_hide", 0.5f, true );
     }
 
 
